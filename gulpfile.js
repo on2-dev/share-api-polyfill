4f6a121a892cdcb1d25e281da5f0e2503afbbbe0
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2019-01-31 12:28:29 -0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+
+const distPath = 'dist/';
+const srcPath = 'src/';
+
+//     concat = require('gulp-concat'),
+//     rename = require('gulp-rename'),
+//     cleanCSS = require('gulp-clean-css'),
+//     htmlmin = require('gulp-htmlmin'),
+//     replace = require('gulp-replace'),
+//     insert = require('gulp-insert'),
+//     minify = require('gulp-babel-minify'),
+//     clean = require('gulp-clean'),
+//     merge = require('merge2');
+const minify = require('gulp-minify');
+const connect = require('gulp-connect');
+const watch = require('gulp-watch');
+
+gulp.task('watch', async function() {
+    gulp.src(srcPath + '*.js')
+        .pipe(gulp.watch(srcPath + '*.js', ['minifyJs']));
+})
+
+gulp.task('webserver', async function() {
+    connect.server({
+      livereload: true,
+      root: ['.', 'dist']
+    });
+});
+   
+gulp.task('livereload', async function() {
+    gulp.src(['dist/scripts/*.js'])
+      .pipe(watch())
+      .pipe(connect.reload());
+});
+
+gulp.task('minifyJs', async function () {
+    gulp.src([srcPath + '/*.js'])
+        .pipe(minify())
+        .pipe(gulp.dest('dist'))
+});
+
+// gulp.task('default', gulp.parallel(['minifyJs', 'livereload', 'watch']));
+gulp.task('default', gulp.parallel(['minifyJs']));
