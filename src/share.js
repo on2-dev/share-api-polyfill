fd3dbb393ed13745b7d957e86f686521de44f388
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2020-10-14 00:37:04 -0300

    Removed support for ie8

--- src/share.js
+++ src/share.js
@@ -193,12 +193,7 @@ navigator.share = navigator.share || (function () {
 			style = document.createElement('style');
 
 		style.type = 'text/css';
-		if (style.styleSheet) {
-			// This is required for IE8 and below.
-			style.styleSheet.cssText = css;
-		} else {
-			style.appendChild(document.createTextNode(css));
-		}
+		style.appendChild(document.createTextNode(css));
 
 		style.id = 'shareAPIPolyfill-style';
 
